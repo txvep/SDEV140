1582730c7610bf3efa5021ffd6d23c4001f41080
txvep <epalos@ivytech.edu>
2026-09-20 00:39:01 -0400

    mid

---
+++ PalosEverardoM4_Ch7Ex1.py
@@ -0,0 +1,7 @@
+"""Design a program that asks the user to enter a store’s sales for each day of the week. 
+The amounts should be stored in a list. 
+Use a loop to calculate the total sales for the week and display the result."""
+
+def main():
+    input("sales for the day")
+    
